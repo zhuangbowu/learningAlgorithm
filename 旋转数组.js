49878225a0d2035709c64a447e70fc11dbf84d52
1369581758@qq.com <123456>
2022-01-09 21:07:21 +0800

    2022年1月9日21:07:06 旋转数组算法留着以后

---
+++ 旋转数组.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const rotate = function (nums, k) {
+    console.log(nums, k);
+};
+
+rotate(nums = [1, 2, 3, 4, 5, 6, 7], k = 3);
+rotate(nums = [-1, -100, 3, 99], k = 2);
