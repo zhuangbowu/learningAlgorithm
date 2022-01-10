24f898bb58c4feed363a9d48be2ead267e0f6481
庄博伍 <zhuangbw@accplat.com>
2022-01-10 10:17:07 +0800

    2022年1月10日10:16:57 写完重置为最开始的样子

--- 买卖股票的最佳时机2.js
+++ 买卖股票的最佳时机2.js
@@ -3,14 +3,7 @@
  * @return {number}
  */
 let maxProfit = function (prices) {
-    let profit = 0;
-    for (let i = 1; i < prices.length; i++) {
-        // 当天交易值比前一天的交易值大、那么累加利润
-        if (prices[i] > prices[i - 1]) {
-            profit += prices[i] - prices[i - 1];
-        }
-    }
-    return profit;
+
 };
 // 可以重复购买的情况下输出最大利润
 console.log(maxProfit(prices = [7, 1, 5, 3, 6, 4]));
--- 数组的去重操作.js
+++ 数组的去重操作.js
@@ -3,44 +3,10 @@
  * @return {number}
  */
 let removeDuplicates = function (nums) {
-    // 判断传入不是数组返回长度为0
-    if (!(nums instanceof Array)) {
-        return 0;
-    }
-    // 判断数组长度小于/等于1的时候直接返回数组长度
-    if (nums.length <= 1) {
-        return nums.length;
-    }
-    let i = nums.length - 1;
-    for (let j = nums.length; j > 0; j--) {
-        if (nums[j] === nums[i]) {
-            nums.splice(j, 1)
-        }
-        i--;
-    }
-    return nums.length;
-};
-let removeDuplicatesTwo = function (nums) {
-    // 判断传入不是数组返回长度为0
-    if (!(nums instanceof Array)) {
-        return 0;
-    }
-    // 判断数组长度小于/等于1的时候直接返回数组长度
-    if (nums.length <= 1) {
-        return nums.length;
-    }
-    let i = 1, j = 1;
-    while (j < nums.length) {
-        if (nums[j] !== nums[j - 1]) {
-            nums[i] = nums[j];
-            i++;
-        }
-        j++;
-    }
-    return i;
+    
 };
 
 // 输出数组长度
-console.log(removeDuplicatesTwo([0, 0, 0, 0, 0, 1, 1, 2, 2, 3, 3]));
-console.log(removeDuplicatesTwo([1, 2, 5, 9, 12]));
-console.log(removeDuplicatesTwo('sda'));
+console.log(removeDuplicates([0, 0, 0, 0, 0, 1, 1, 2, 2, 3, 3]));
+console.log(removeDuplicates([1, 2, 5, 9, 12]));
+console.log(removeDuplicates('sda'));
