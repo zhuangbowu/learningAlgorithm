4dcc05cc26ce15d76538dfd85a9c6fdff8c2313f
1369581758@qq.com <123456>
2022-01-11 23:13:40 +0800

    2022年1月11日23:13:29 第三天的练习

--- 两数之和.js
+++ 两数之和.js
@@ -4,7 +4,15 @@
  * @return {number[]}
  */
 let twoSum = function (nums, target) {
-
+    let arr = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (arr.has(target - nums[i])) {
+            return [arr.get(target - nums[i]), i]
+        } else {
+            arr.set(nums[i], i);
+        }
+    }
+    return [];
 };
 
 // 给定一个整数数组 nums和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那两个整数，并返回它们的数组下标。
@@ -12,12 +20,12 @@ let twoSum = function (nums, target) {
 // 你可以按任意顺序返回答案。
 // 输入：nums = [2,7,11,15], target = 9
 // 输出：[0,1]
-twoSum(nums = [2, 7, 11, 15], target = 9)
+console.log(twoSum(nums = [2, 7, 11, 15], target = 9));
 // 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
 // 输入：nums = [3,2,4], target = 6
 // 输出：[1,2]
-twoSum(nums = [3, 2, 4], target = 6)
+// console.log(twoSum(nums = [3, 2, 4], target = 6));
 
 // 输入：nums = [3,3], target = 6
 // 输出：[0,1]
-twoSum(nums = [3, 3], target = 6)
+// console.log(twoSum(nums = [3, 3], target = 6));
